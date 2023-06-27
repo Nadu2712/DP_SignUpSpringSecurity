3fbc12dae7e05ac12b6c432528eb092e38d3e827
Nadushanth Sathiskumar <snadushanth2000@gmail.com>
2023-06-27 11:25:58 +0530

    Initial Commit

---
+++ src/main/java/com/example/dp_signupspringsecurity/DpSignUpSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.dp_signupspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpSignUpSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpSignUpSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dp_signupspringsecurity/DpSignUpSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dp_signupspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpSignUpSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
